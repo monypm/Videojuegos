e876ebe90c367970554c9fd30e0bf284412737dc
Mónica <monica.p.m@outlook.com>
2017-04-03 12:35:09 -0500

    Se agregó loq ue se tenía de la segunda entrega

---
+++ PrototipoJuego/Assets/Scripts/Buttons.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour {
+
+
+
+	public void BotonJugar(){
+		Application.LoadLevel ("Escena1");
+	}
+
+	public void BotonSalir(){
+		Application.Quit ();
+	}
+}
