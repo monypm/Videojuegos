2939d73beb6cddabe63cbacffb66b855fa2ca485
Mónica <monica.p.m@outlook.com>
2017-04-26 22:57:04 -0500

    Se agregó la escena del video, con el video, y las transiciones entre escenas funcionan bien

---
+++ PrototipoJuego/Assets/Scripts/IntroVidPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class IntroVidPlayer : MonoBehaviour {
+
+	public MovieTexture movie;
+	private bool started = false;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<RawImage> ().texture = movie as MovieTexture;
+		movie.Play ();
+		started = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!movie.isPlaying && started) {
+			SceneManager.LoadScene ("Escena1");
+		}
+	}
+}
